91c6d4056b3d9f0a191141f4b3d96b8bb7c20b38
Matthew Campbell <mjcampbell74@gmail.com>
2022-09-25 10:08:36 -0400

    init commit

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return <div class="text-2xl bg-slate-900 text-green-500">hyperbolic</div>;
+}
