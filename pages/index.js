c9e11c6c5ed14720b3a33573ce71c282c1b46263
Matthew Campbell <mjcampbell74@gmail.com>
2022-09-25 10:33:24 -0400

    added image

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,13 @@
 import Head from "next/head";
 import Image from "next/image";
+import pic from "../public/hyperbolic-trailer.png";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  return <div class="text-2xl bg-slate-900 text-green-500">hyperbolic</div>;
+  return (
+    <div>
+      <div class="text-2xl text-green-500">hyperbolic</div>
+      <Image src={pic}></Image>
+    </div>
+  );
 }
